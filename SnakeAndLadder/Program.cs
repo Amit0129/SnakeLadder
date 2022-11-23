9c58eb92bb04bf2093b9f82fead43f1f5a71f9ea
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-23 19:28:27 +0530

    Added:UC1 Played With Single Player With Position zero

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake And Ladder Game");
+
+            int position = 0;
+            Console.WriteLine("Playear position now is {0}",position);
         }
     }
 }
