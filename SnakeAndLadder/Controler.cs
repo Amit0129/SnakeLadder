5d562284e328daab00384d8ee2833458c4039ad5
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-23 20:03:01 +0530

    Added:UC3 The Player then checks for a Option. They are No Play,Ladder or Snake. - Use ((RANDOM)) to check

---
+++ SnakeAndLadder/Controler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Controler
+    {
+        public int NoPlay(int position)
+        {
+            Console.WriteLine( $"The Player position is : {position}");
+            return position;
+        }
+        public int Ladder(int position, int roll)
+        {
+            position += roll;
+            Console.WriteLine("Playear position now is {0}", position);
+            return position;
+        }
+        public int Snake(int position, int roll)
+        {
+            position -= roll;
+            if (position < 0)
+            {
+                position = 0;
+            }
+            Console.WriteLine("Playear position now is {0}", position);
+            return position;
+        }
+    }
+}
--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -12,6 +12,23 @@
             Random random = new Random();
             int rollDice = random.Next(1, 7);
             Console.WriteLine($"The value of dice after roll is : {rollDice}");
+
+            int checkOption = random.Next(1, 4);
+            Console.WriteLine("Check Option is {0}",checkOption);
+            Controler controler = new Controler();
+
+            switch (checkOption)
+            {
+                case 1:
+                    controler.NoPlay(position);
+                    break;
+                case 2:
+                    controler.Ladder(position, rollDice);
+                    break;
+                default:
+                    controler.Snake(position, rollDice);
+                    break;
+            }
         }
     }
 }
