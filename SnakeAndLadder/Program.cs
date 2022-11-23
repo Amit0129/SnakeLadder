6046abda783f38183a93a02dec7b8eb8b50a54db
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-23 20:26:03 +0530

    Added:UC4 Repeat till the Player reaches the winning position 100. - Note In case the player

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,27 +7,36 @@
             Console.WriteLine("Welcome to Snake And Ladder Game");
 
             int position = 0;
-            Console.WriteLine("Playear position now is {0}",position);
-
+            Console.WriteLine("Playear position now is {0}", position);
             Random random = new Random();
-            int rollDice = random.Next(1, 7);
-            Console.WriteLine($"The value of dice after roll is : {rollDice}");
 
-            int checkOption = random.Next(1, 4);
-            Console.WriteLine("Check Option is {0}",checkOption);
-            Controler controler = new Controler();
-
-            switch (checkOption)
+            while (position < 100)
             {
-                case 1:
-                    controler.NoPlay(position);
-                    break;
-                case 2:
-                    controler.Ladder(position, rollDice);
-                    break;
-                default:
-                    controler.Snake(position, rollDice);
-                    break;
+
+
+                int rollDice = random.Next(1, 7);
+                Console.WriteLine($"The value of dice after roll is : {rollDice}");
+
+                int checkOption = random.Next(1, 4);
+                Console.WriteLine("Check Option is {0}", checkOption);
+                Controler controler = new Controler();
+
+                switch (checkOption)
+                {
+                    case 1:
+                        //controler.NoPlay(position);
+                        position = controler.NoPlay(position);
+
+                        break;
+                    case 2:
+                        //controler.Ladder(position, rollDice);
+                        position = controler.Ladder(position, rollDice);
+                        break;
+                    case 3:
+                        //controler.Snake(position, rollDice);
+                        position = controler.Snake(position, rollDice);
+                        break;
+                }
             }
         }
     }
