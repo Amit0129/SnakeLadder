d387a99f792ca90a22ef1a022f9ef200a8575532
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-23 19:37:00 +0530

    Added:UC2 Use RANDOM to get the number between 1 to 6

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -8,6 +8,10 @@
 
             int position = 0;
             Console.WriteLine("Playear position now is {0}",position);
+
+            Random random = new Random();
+            int rollDice = random.Next(1, 7);
+            Console.WriteLine($"The value of dice after roll is : {rollDice}");
         }
     }
 }
